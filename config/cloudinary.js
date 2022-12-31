df2c53715baf8fe4582482c5aa396cb572115121
Gonuguntla Udaya Kiran <102300201+udaya19@users.noreply.github.com>
2022-12-31 12:25:27 +0530

    mongodb and cloudinary setup

---
+++ config/cloudinary.js
@@ -0,0 +1,8 @@
+const cloudinary = require("cloudinary").v2;
+const clodinaryConfig = cloudinary.config({
+  cloud_name: "dtoguorry",
+  api_key: "528411381656446",
+  api_secret: "hhESrXn2l4bVAaEtok_b2SXengQ",
+});
+
+module.exports = clodinaryConfig;
--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 const express = require("express");
-const app = express();
+const dotenv = require("dotenv").config();
+const bodyParser = require("body-parser");
+const { db } = require("./config/database");
+const clodinaryConfig = require("./config/cloudinary");
 
+const app = express();
 app.use(express.json());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use("/api/users", require("./routes/user"));
 
-const port = 3001;
+const port = process.env.PORT;
 app.listen(port, () => {
   console.log(`Port running on port ${port}`);
 });
