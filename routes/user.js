de324ad217c38a642c5bb196e1b89ef1530ddf9e
Gonuguntla Udaya Kiran <102300201+udaya19@users.noreply.github.com>
2023-01-01 13:06:57 +0530

    changes w.r.t profile route

--- routes/user.js
+++ routes/user.js
@@ -124,10 +124,23 @@ router.post("/login", uploader.none(), async (req, res) => {
 });
 
 router.get("/profile", isAuthenticated, async (req, res) => {
-  return res.status(200).json({
-    success: true,
-    user: req.user,
-  });
+  try {
+    const user = await User.findById(req.user._id);
+    if (!user) {
+      return res.status(404).json({
+        error: "User not found",
+      });
+    }
+    return res.status(200).json({
+      success: true,
+      user,
+    });
+  } catch (error) {
+    return res.json({
+      success: false,
+      error: error.message,
+    });
+  }
 });
 
 module.exports = router;
