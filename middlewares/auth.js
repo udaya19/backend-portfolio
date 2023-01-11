c7a8ebcedf2639336c91dc7839a14da35ee4a5f0
Gonuguntla Udaya Kiran <102300201+udaya19@users.noreply.github.com>
2023-01-11 16:55:13 +0530

    changes w.r.t authentication

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -7,14 +7,14 @@ exports.isAuthenticated = async (req, res, next) => {
     const token = req.headers.authorization?.split("Bearer ")[1];
     if (!token) {
       return res.json(400, {
-        message: "please login to create a post",
+        message: "please login to perform this action",
       });
     }
     const decoded = await jwt.verify(token, process.env.JWT_SECRET);
     req.user = await User.findById(decoded._id);
     next();
   } catch (error) {
-    res.json(500, {
+    res.status(500).json({
       message: error.message,
     });
   }
